bb878241a9abd013fd0a36cffcf00f2ebc20f7dd
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-01-20 01:45:37 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 172 ms, Memory: 16944 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,45 +1,40 @@
-import java.util.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 소수 리스트 만들기
+        boolean[] prime = new boolean[10_001];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+        for (int i=2; i*i <= 10_000; i++){
+            if (prime[i]){
+                for (int j=i*i; j<=10_000; j+=i) prime[j] = false;
+            }
+        }
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
-        int [] num = new int[T];
-        int maxNum = Integer.MIN_VALUE;
-        for (int i=0;i<T;i++){
-            num[i] = Integer.parseInt(br.readLine());
-            maxNum = Math.max(maxNum, num[i]);
-        }
-        // 소수 리스트 만들기
-        boolean[] prime = new boolean[maxNum+1];
-        Arrays.fill(prime, true);
-        prime[0] = prime[1] = false;
-        for (int i=2; i*i <= maxNum; i++){
-            if (prime[i]){
-                for (int j=i*i; j<=maxNum; j+=i) prime[j] = false;
-            }
-        }
-        ArrayList<Integer> primes = new ArrayList<>();
-        for (int i=2;i<=maxNum;i++){
-            if(prime[i]) primes.add(i);
-        }
 
+        Loop1:
         for (int i=0;i<T;i++){
-            if (primes.contains(num[i]/2)) bw.write(num[i]/2+" "+num[i]/2+"\n");
+            int n = Integer.parseInt(br.readLine());
+            if (prime[n/2]) bw.write(n/2+" "+n/2+"\n");
             else {
-                for (int j=num[i]/2; j>=2; j--){
-                    if (primes.contains(j)){
-                        int diff = num[i] - j;
-                        if (primes.contains(diff)) {
-                            bw.write(j+" "+diff+"\n");
-                            break;
+                for (int k=n/2; k>=2; k--){
+                    if (prime[k]){
+                        int diff = n - k;
+                        if (prime[diff]) {
+                            bw.write(k+" "+diff+"\n");
+                            continue Loop1;
                         }
                     }
                 }
             }
         }
+
         bw.flush();
         bw.close();
     }
