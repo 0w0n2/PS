af2fd3de5ffc1d7a5e6cccc2eca9e8dd404c070b
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-01-21 22:32:03 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -2,6 +2,4 @@ while(True):
     ip = input()
     if (ip=="# 0 0"): break
     name, age, weight = ip.split()
-    age, weight = map(int, [age, weight])
-    if(age>17 or weight>=80): print(f'{name} Senior')
-    else: print(f'{name} Junior')
+    print(f"{name} {'Senior' if int(age)>17 or int(weight)>=80 else 'Junior'}")
