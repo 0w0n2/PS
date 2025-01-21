1db25de3752ce5498825978e1fd215a581d151e9
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-01-21 20:59:17 +0900

    [Bronze IV] Title: 모음의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.py
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -1,8 +1,4 @@
-word = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
 while(True):
     ip=input()
     if(ip=="#"): break
-    sum = 0
-    for i in range(len(word)):
-        sum += ip.count(word[i])
-    print(sum)
+    print(sum(map(ip.count, 'aAeEiIoOuU')))
