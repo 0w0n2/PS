b84ba0dc56c47f513a4f470597c6fcbd14779281
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-01-29 12:29:19 +0900

    [Silver II] Title: 좌표 압축, Time: 1864 ms, Memory: 265776 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] original = new int[n];
+        int[] sorted = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0;i<n;i++) original[i] = sorted[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(sorted); // 내림차순 정렬
+
+        HashMap<Integer, Integer> rankMap = new HashMap<>(); // 정렬된 서로 다른 숫자가 각각 몇 번째 등수인지 기록 (중복 숫자는 같은 등수라 생략하기 위해 해시맵 씀)
+        int rank = 0; 
+        for (int num : sorted){
+            if (!rankMap.containsKey(num)) rankMap.put(num, rank++); // 후위증가
+        }
+        for (int num : original) bw.write(rankMap.get(num)+" "); // 맵에서 해당 숫자가 몇 번째 등수인지 찾고, 출력
+
+        bw.flush();
+        bw.close();
+    }
+}
