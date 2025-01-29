9bf9d6c4ec21cfea1cb84d8f49b85c584ec03a20
0w0n2 <dahxtq1@naver.com>
2025-01-29 12:38:45 +0900

    250129

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # 폴더 전체를 이전에 지정해둔 remote 주소(브랜치 = master)로 전체 백업하는 스크립트
-# Usage : ./backoff.sh <commit message> (Ex/ .backoff.sh 250116)
+# Usage (default / commit message : today's data) : ./backup.sh (Ex/ .backup.sh)
+
+# If you want to use a custom commit message, uncomment line 20 and use "line 21".
+# Usage 2 (for custom commit message) : ./backup.sh <commit message> (Ex/ .backup.sh 250116)
 
 # git의 줄바꿈 설정을 변경(환경설정)
 git config --global core.autocrlf false
@@ -9,7 +12,6 @@ git config --global core.autocrlf false
 git remote -v
 
 # commit message = today's date
-# commit message는 오늘의 날짜로 설정해둠. 직접 설정하고 싶다면 18줄 지우고 19줄 쓰세염
 message=$(date +"%y%m%d")
 echo "commit message : $message"
 
