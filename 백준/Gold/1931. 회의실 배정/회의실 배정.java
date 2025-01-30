47744b57e183fa11ea17bb3cf72c839ee761efb5
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-01-30 16:24:29 +0900

    [Gold V] Title: 회의실 배정, Time: 440 ms, Memory: 41804 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,42 +1,29 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int numMeeting = Integer.parseInt(br.readLine());
-        int [][] meetingTime = new int [numMeeting][2];
-        for (int i = 0; i < meetingTime.length; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            meetingTime[i][0] = Integer.parseInt(st.nextToken()); // 시작 시간
-            meetingTime[i][1] = Integer.parseInt(st.nextToken()); // 종료 시간
-        } // 입력 끝
-
-        Arrays.sort(meetingTime, new Comparator<int[]>() {
-            @Override
+        int n = Integer.parseInt(br.readLine()); // 회의 개수
+        PriorityQueue<int []> meet = new PriorityQueue<>(new Comparator<int[]>() {
             public int compare(int[] o1, int[] o2) {
-                return o1[1]!=o2[1] ? o1[1]-o2[1] : o1[0]-o2[0];
+                return o1[1]!=o2[1]? o1[1]-o2[1]:o1[0]-o2[0];
             }
-        }); // 종료 시간 빠른 기준으로 배열 정렬
-
-        int count = 1;
-        int currentLastTime = meetingTime[0][1];
-        for (int i = 1; i < numMeeting; i++) {
-            if (meetingTime[i][0]>=currentLastTime){    // 회의 시간이 안 겹칠 때(현재 등록된 회의의 끝나는 시간 <= 다음 예정 회의의 시작 시간), 가장 빨리 끝나는 회의를 예정 회의로 등록
-                currentLastTime = meetingTime[i][1];
-                count++;
+        }); // 큐 정렬 1순위 : 회의 종료 빠른 순([1]), 2순위 : 회의 시작 빠른 순([0])
+        for (int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            meet.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
+        int res = 1; // 결과
+        int end = meet.poll()[1]; // 첫 회의의 종료 시간 저장
+        int size = meet.size(); // 남은 미팅 후보 개수
+        for (int i=0;i<size;i++){
+            int[] nowMeeting = meet.poll(); // 미팅 선택
+            if (end<=nowMeeting[0]){ 
+                res++;
+                end = nowMeeting[1]; // 해당 미팅의 종료 시간으로 업데이트
             }
         }
-        System.out.println(count);
-
-
-
+        System.out.println(res);
     }
 }
