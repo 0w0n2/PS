3720af7e18a1f5dca590e6a3a14ec87389dd1e3a
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-02-21 00:19:50 +0900

    [Gold IV] Title: 플로이드, Time: 372 ms, Memory: 43532 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * 모든 정점 쌍(u, v)에 대해 어떤 정점 a를 중간 정점으로 해서 최단 경로를 갱신할 수 있는지? -> dp를 활용
+ * -> u->v 까지의 거리보다 u->a->v가 더 짧으면 최단 경로를 후자로 갱신
+ */
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		int INF = 1000000000;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()); // 도시 개수(노드)
+		int m = Integer.parseInt(br.readLine()); // 버스의 개수(간선)
+		// 모든 (i, j)쌍에 대하여 최소 비용 경로 계산
+		int[][] dist = new int [n+1][n+1];
+		for (int i=1;i<=n;i++) {
+			for (int j=1;j<=n;j++) {
+				dist[i][j] = i==j ? 0 : INF; // 같은 정점끼리의 이동 거리는 0
+			}
+		}
+
+		StringTokenizer st;
+		for (int i=0;i<m;i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			dist[u][v] = Math.min(dist[u][v], w);
+		}
+		
+		// 플로이드-워셜
+		for (int a=1; a<=n; a++) {
+			for (int i=1;i<=n;i++) for (int j=1;j<=n;j++) {
+					if (dist[a][j]!=INF && dist[i][a]!=INF && dist[a][j] + dist[i][a] < dist[i][j]) dist[i][j] = dist[a][j] + dist[i][a]; // 갱신
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i=1;i<=n;i++) {
+			for (int j=1;j<=n;j++) {
+				if (dist[i][j]==INF) sb.append(0).append(" ");
+				else sb.append(dist[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+}
