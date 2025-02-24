c3a106ffd2b55779c90625628acb00e388e91da2
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-02-24 23:23:54 +0900

    [Gold I] Title: 최종 순위, Time: 1208 ms, Memory: 123808 KB -BaekjoonHub

---
+++ 백준/Gold/3665. 최종 순위/최종 순위.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine()); // 테스트 케이스
+		StringBuilder sb = new StringBuilder(); // 출력용
+		
+		Loop1:
+		for (int t=0;t<tc;t++) {
+			
+			int n = Integer.parseInt(br.readLine()); // 팀의 수(노드 수)
+			ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+			for (int i=0;i<=n;i++) map.add(new ArrayList<>());
+			
+			int[] lastYear = new int[n];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i=0;i<n;i++) {
+				lastYear[i] = Integer.parseInt(st.nextToken()); 
+			}
+			
+			int[] inDegree = new int[n+1];
+			for (int i=0;i<n;i++) {
+				for (int j=i+1;j<n;j++) {
+					map.get(lastYear[i]).add(lastYear[j]);
+					inDegree[lastYear[j]]++;
+				}
+			}
+			
+			int m = Integer.parseInt(br.readLine());
+			for (int i=0;i<m;i++) {
+				st = new StringTokenizer(br.readLine());
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
+				
+				if (map.get(start).remove(Integer.valueOf(end))) { // 원래 순서 start->end 였을 경우, end->start로 반전
+			        map.get(end).add(start);
+			        inDegree[end]--;
+			        inDegree[start]++;
+			    } else { // 원래 순서 end->start 였을 경우, start->end로 반전
+			        map.get(start).add(end);
+			        map.get(end).remove(Integer.valueOf(start));
+			        inDegree[start]--;
+			        inDegree[end]++;
+			    }
+			}
+			
+			Deque<Integer> q = new ArrayDeque<>(); // 큐
+			int[] result = new int[n];
+			
+			for (int i=1;i<=n;i++) {
+				if(inDegree[i]==0) q.offerLast(i);
+			}
+			
+			// 위상 정렬 시작
+			StringBuilder temp = new StringBuilder();
+			for (int i=0;i<n;i++) {
+				// 도중에 큐가 비면 정렬 불가능 (순환 고리 생김)
+				if(q.isEmpty()) {
+					sb.append("IMPOSSIBLE").append("\n");
+					continue Loop1; // 정렬 종료
+				}
+				
+				int current = q.pollFirst();
+				result[i] = current;
+				temp.append(result[i]).append(" ");
+				
+				if (i==(n-1)) sb.append(temp.toString().trim()).append("\n");
+				
+				for (int nextPoint:map.get(current)) {
+					if (--inDegree[nextPoint]==0) q.addLast(nextPoint);
+				}
+			}
+		}
+		System.out.println(sb);
+	}
+}
