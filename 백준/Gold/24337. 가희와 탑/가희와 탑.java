7a9698491e34345100d3d851ed74347714a9a595
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-03-07 22:20:09 +0900

    [Gold III] Title: 가희와 탑, Time: 228 ms, Memory: 26528 KB -BaekjoonHub

---
+++ 백준/Gold/24337. 가희와 탑/가희와 탑.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); // 건물 개수
+		int a = Integer.parseInt(st.nextToken()); // 가희가 볼 수 있는
+		int b = Integer.parseInt(st.nextToken()); // 단비가 볼 수 있는
+		
+		ArrayList<Integer> list = check(N, a, b);
+		
+		if (list.size()>N) System.out.println(-1);
+		else {
+			StringBuilder sb = new StringBuilder();
+			sb.append(list.get(0)).append(" "); // 맨 앞 건물
+			for (int i=0;i<N-list.size();i++) sb.append(1).append(" ");	
+			// 맨 앞 건물에 가려져서 안 보이는 건물 -> 1로(사전순으로 앞섬) 설정해서 맨 앞 건물에 모두 가려버림  
+			
+			for (int i=1;i<list.size();i++) sb.append(list.get(i)).append(" "); // (~끝까지)
+			
+			System.out.println(sb);
+		}
+	}
+	
+	static ArrayList<Integer> check(int N, int a, int b) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i=1;i<a;i++) list.add(i); // 가희 쪽 채우기
+		list.add(Math.max(a, b));  // 둘 사이 가장 높은 탑(가희가 마지막으로 보는 가장 높은 건물)
+		while(b>1) list.add(-1 + b--); // 가장 높은 탑 이후로 단비 쪽 채우기
+		
+		return list;
+	}
+}
