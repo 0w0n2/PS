be568a1b5c02cc966d094a137ba14b4151b27a2d
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-03-07 22:33:59 +0900

    [Gold III] Title: 가희와 탑, Time: 224 ms, Memory: 26252 KB -BaekjoonHub

--- 백준/Gold/24337. 가희와 탑/가희와 탑.java
+++ 백준/Gold/24337. 가희와 탑/가희와 탑.java
@@ -10,10 +10,10 @@ public class Main {
 		int a = Integer.parseInt(st.nextToken()); // 가희가 볼 수 있는
 		int b = Integer.parseInt(st.nextToken()); // 단비가 볼 수 있는
 		
-		ArrayList<Integer> list = check(N, a, b);
-		
-		if (list.size()>N) System.out.println(-1);
+		if (a+b-1>N) System.out.println(-1);
 		else {
+			ArrayList<Integer> list = check(N, a, b);
+			
 			StringBuilder sb = new StringBuilder();
 			sb.append(list.get(0)).append(" "); // 맨 앞 건물
 			for (int i=0;i<N-list.size();i++) sb.append(1).append(" ");	
@@ -28,8 +28,8 @@ public class Main {
 	static ArrayList<Integer> check(int N, int a, int b) {
 		ArrayList<Integer> list = new ArrayList<>();
 		for (int i=1;i<a;i++) list.add(i); // 가희 쪽 채우기
-		list.add(Math.max(a, b));  // 둘 사이 가장 높은 탑(가희가 마지막으로 보는 가장 높은 건물)
-		while(b>1) list.add(-1 + b--); // 가장 높은 탑 이후로 단비 쪽 채우기
+		list.add(Math.max(a, b));  // 둘 사이 가장 높은 탑(가희와 단비가 공통으로 제일 마지막에 보는 건물)
+		for (int i=b-1;i>0;i--) list.add(i); // 가장 높은 탑 이후로 단비 쪽 채우기
 		
 		return list;
 	}
