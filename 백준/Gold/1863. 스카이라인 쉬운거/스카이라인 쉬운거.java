8319d42ea21ed4370147b026e742a4d27070c14b
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-03-11 05:46:57 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 172 ms, Memory: 19520 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -3,20 +3,12 @@ import java.io.*;
 
 public class Main {
 	
-	static class Node{
-		int x, height;
-		Node(int x, int height){
-			this.x = x;
-			this.height = height;
-		}
-	}
-	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()); 
 		// N개의 줄에 고도가 바뀌는 지점의 x, y 좌표가 주어짐
 		
-		Deque<Node> stack = new ArrayDeque<>();
+		Deque<Integer> stack = new ArrayDeque<>();
 		
 		StringTokenizer st;
 	
@@ -27,27 +19,27 @@ public class Main {
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			
-			Node top; 
-			if (!stack.isEmpty() && y < (top=stack.getLast()).height) {
+			int top; 
+			if (!stack.isEmpty() && y < (top=stack.getLast())) {
 				// 더 작은 높이가 들어왔다면, 현재 들어온 값보다 같거나 작은 높이의 stack의 원소가 나오기 전까지 계산
 				top = stack.pollLast();
 				building++;
-				while(!stack.isEmpty() && stack.getLast().height > y) {
-					Node topleft = stack.pollLast();
-					if (top.height != topleft.height) building++;
+				while(!stack.isEmpty() && stack.getLast() > y) {
+					int topleft = stack.pollLast();
+					if (top != topleft) building++;
 					top = topleft;
 				}
 			}
-			if(y!=0) stack.offerLast(new Node(x, y));
+			if(y!=0) stack.offerLast(y);
 			
 		}
 		
 		if (!stack.isEmpty()) {
-			Node top = stack.pollLast();
+			int top = stack.pollLast();
 			building++;
 			while(!stack.isEmpty()) {
-				Node topleft = stack.pollLast();
-				if (top.height!=topleft.height) building++;
+				int topleft = stack.pollLast();
+				if (top!=topleft) building++;
 				top = topleft;
 			}
 		}
