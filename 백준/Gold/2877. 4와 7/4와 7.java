a4e7069496c485217d50fafc1afdae5fbefb1767
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-03-20 21:34:28 +0900

    [Gold V] Title: 4와 7, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.java
+++ 백준/Gold/2877. 4와 7/4와 7.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
@@ -5,17 +6,41 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int x = Integer.parseInt(br.readLine());
-		int n = (int)(Math.log(x+1)/Math.log(2)); // 자릿수 : log2(x+1) 
-		int diff = x - ((int)Math.pow(2, n) - 1); // x번째 숫자가 해당 자릿수(수열) 내에서 어떤 위치에 있는지
-		String b = Integer.toBinaryString(diff);  // 이진수로 변환
+		n = (int)(Math.log(x+1)/Math.log(2)); // 자릿수
 		
+		
+		int diff = x - ((int)Math.pow(2, n) - 1);
+		String b = Integer.toBinaryString(diff);
 		StringBuilder sb = new StringBuilder();
 		
-		for (int i=0;i<n-b.length();i++) sb.append(4); // 구한 이진수에서 부족한 숫자는 0(4)로 채움
-		for (int i=0;i<b.length();i++) {			   // 0이면 4, 1이면 7 추가  
+		for (int i=0;i<n-b.length();i++) sb.append(4);
+		for (int i=0;i<b.length();i++) {
 			if (b.charAt(i)=='0') sb.append(4);
 			else sb.append(7);
 		}
 		System.out.println(sb);
+		
 	}
+	
+	static int[] num = {4,7}; 
+	static int n;
+	static int diff;
+	static boolean alreadyPrinted = false;
+	static boolean[] isUsed = new boolean[2];
+	
+	static void permutation(int ct, int x) {
+		if (alreadyPrinted) return;
+		
+		if (ct==n) {
+			if (diff--==0) {
+				System.out.print(x);
+				alreadyPrinted = true;
+			}
+			return;
+		}
+		
+		permutation(ct+1, x+4*(int)Math.pow(10, n-ct-1));
+		permutation(ct+1, x+7*(int)Math.pow(10, n-ct-1));
+	}
+	
 }
