d7bcf231aacb96e887441af44e2b56eb07b4ebaa
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-03-24 22:17:31 +0900

    [Gold II] Title: 철로, Time: 624 ms, Memory: 48320 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.java
+++ 백준/Gold/13334. 철로/철로.java
@@ -3,12 +3,18 @@ import java.io.*;
 
 public class Main {
 	
+	/**
+	 * @since 2025-03-24
+	 * @author 0w0n
+	 * Java 11 :  
+	 */
+	
 	static class Node implements Comparable<Node>{
 		int start, end, len;
 		Node(int start, int end){
 			this.start = start;
 			this.end = end;
-			this.len = Math.abs(this.start-this.end);
+			this.len = this.end-this.start;
 		}
 		
 		@Override
@@ -24,37 +30,37 @@ public class Main {
 		
 		Node[] H = new Node[n]; // 집 위치 저장
 		
-		int minLen = Integer.MAX_VALUE; // 
-		for (int i=0;i<n;i++) {
+		int minLen = Integer.MAX_VALUE; 
+		for (int i=0;i<n;i++) {	// O(n)
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			H[i] = new Node(Math.min(a, b), Math.max(a, b)); // 집과 사무실 위치 입력 받음
-			minLen = Math.min(minLen, H[i].len); // 모든 집에 대해 집과 사무실의 최소 거리를 계산
+			minLen = Math.min(minLen, H[i].len);
 		}
 		
 		int d = Integer.parseInt(br.readLine()); // 철로의 길이
-		if (d<minLen) { // 만약 집과 사무실 위치를 입력 받는 단계에서 구한 집의 최소 길이가 철로의 길이보다 크다면 0 출력 후 조기 종료 
+		if (minLen>d) { // 조기종료
 			System.out.print(0);
 			return;
 		}
 		
-		Arrays.sort(H); // 집 좌표 정렬
+		Arrays.sort(H); // 집 좌표 정렬 // O(nlogn)
 		
 		int maxElement = 0; 
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2)->o1.start-o2.start);
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		
-		for (int i=0;i<n;i++) {
-			if (H[i].len<=d) {
-				while(!pq.isEmpty() && H[i].end-pq.peek().start>d) {
-					// 현재 pq의 peek 좌표의 start (= pq에 들어있는 좌표 정보 중 가장 왼쪽에 치우친 집의 시작점 점) 값과 현재 i번째 집의 끝 점 간의 길이가 d 안에 포함될 때까지 pq의 원소를 제거함
-					pq.poll();
-				}
-				pq.add(H[i]); // i번째 집을 pq에 삽입
-				maxElement = Math.max(pq.size(), maxElement); 
-				// 현재 pq에 있는 원소 개수 = 집과 사무실 위치가 모두 길이 d인 선분에 포함되는 사람들의 수
+		for (int i=0;i<n;i++) { // O(n) -> O(nlogn) (내부 로직 포함)
+			if (H[i].len>d) continue;
+			while(!pq.isEmpty() && H[i].end-pq.peek()>d) { 
+				// 현재 pq의 peek 좌표의 start (= pq에 들어있는 좌표 정보 중 가장 왼쪽에 치우친 집의 시작점 점) 값과 현재 i번째 집의 끝 점 간의 길이가 d 안에 포함될 때까지 pq의 원소를 제거함
+				pq.poll(); // O(logn)
 			}
+			pq.add(H[i].start); // i번째 집을 pq에 삽입 // O(logn)
+			maxElement = Math.max(pq.size(), maxElement); 
+			// 현재 pq에 있는 원소 개수 = 집과 사무실 위치가 모두 길이 d인 선분에 포함되는 사람들의 수
 		}
 		System.out.print(maxElement);
+		// O(nlogn)
 	}
 }
