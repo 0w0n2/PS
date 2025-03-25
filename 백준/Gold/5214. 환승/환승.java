abe968c22f7c0b171624ad6538261a0d5995bdf8
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-03-25 22:31:36 +0900

    [Gold II] Title: 환승, Time: 764 ms, Memory: 145784 KB -BaekjoonHub

--- 백준/Gold/5214. 환승/환승.java
+++ 백준/Gold/5214. 환승/환승.java
@@ -24,41 +24,37 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken()); // 한 하이퍼튜브가 서로 연결하는 역의 개수
 		int M = Integer.parseInt(st.nextToken()); // 하이퍼 튜브의 개수
 		// 간선 개수 : K*M
-		
 
 		ArrayList<ArrayList<Integer>> map = new ArrayList<>(); 
-		for (int i=0;i<=N+M;i++) map.add(new ArrayList<>()); // 1~N = 연결된 노드
+		for (int i=0;i<=N+M;i++) map.add(new ArrayList<>()); // 1~N = 노드, N+1~N+M = 하이퍼 링크
 		
 		for (int i=0;i<M;i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j=0;j<K;j++) {
 				int s = Integer.parseInt(st.nextToken());
-				map.get(N+i+1).add(s);
-				map.get(s).add(N+i+1);
+				map.get(N+i+1).add(s); // i번째 하이퍼링크가 노드 s와 연결되어 있음을 추가
+				map.get(s).add(N+i+1); // s번째 노드가 i번째 하이퍼링크와 연결되어 있음을 추가
 			}
 		}
 		
-		Deque<Node> q = new ArrayDeque<>();
-
-		q.offer(new Node(1, 0));
-		boolean[] isVisited = new boolean[N+M+1];
-		isVisited[1] = true; // 시작 지점 방문 처리
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		int[] dist = new int[N+M+1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[1] = 0;
+		pq.offer(new Node(1, 0));
 		
-		while(!q.isEmpty()) {
-			Node current = q.pollFirst();
+		while(!pq.isEmpty()) {
+			Node current = pq.poll();
 			
-			if (current.v==N) {
-				System.out.print(current.ct+1);
-				return;
-			}
+			if (dist[current.v]!=current.ct) continue;
 			
-			for (Integer nextNode : map.get(current.v)) { // current.v 가 가지고 있는 "하이퍼 튜브" 조회
-				if (!isVisited[nextNode]) {
-					isVisited[nextNode] = true; // 방문 처리
-					q.offerLast(new Node(nextNode, (nextNode>N) ? current.ct : current.ct+1));
+			for (Integer nextPoint : map.get(current.v)) { // current.v 가 가지고 있는 하이퍼 튜브 조회
+				if (dist[nextPoint]>current.ct + 1) {
+					dist[nextPoint] = nextPoint>N ? current.ct : current.ct+1;
+					pq.offer(new Node(nextPoint, dist[nextPoint]));
 				}
 			}
 		}
-		System.out.println(-1);
+		System.out.println(dist[N]==Integer.MAX_VALUE ? -1 : dist[N]+1);
 	}
 }
