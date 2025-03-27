a04d903db0d7fd93c3c8e79f4666337ec77689f7
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-03-27 10:11:16 +0900

    [Gold V] Title: 문자열 복사, Time: 112 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Gold/2195. 문자열 복사/문자열 복사.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		String P = br.readLine();
+		
+		
+		StringBuilder makeP = new StringBuilder();
+		int idx = 0;
+		int ct = 0;
+		
+		while(makeP.length()<P.length()) {
+			StringBuilder temp = new StringBuilder();
+			while (idx<P.length()) {
+				temp.append(P.charAt(idx));
+				if (!S.contains(temp)) {
+					temp.delete(temp.length()-1, temp.length());
+					break;
+				}
+				idx++;
+				
+			}
+			makeP.append(temp); 
+			ct++;
+		}
+		System.out.print(ct);
+	}
+}
