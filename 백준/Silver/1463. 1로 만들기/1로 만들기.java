96fd29ab33689949ca85262ad4a7824b187e2687
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-04-03 16:18:01 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 18216 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,17 +1,22 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int [] dp = new int[1_000_001];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[1] = 0; // baseCase
-        for (int i=2;i<=n;i++){ // 바텀업
-            dp[i] = dp[i-1]+1;
-            if(i%2==0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
-            if(i%3==0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
-        }
-        System.out.print(dp[n]);
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // N을 1로 만들기
+		int[] dp = new int[N+1]; // 1부터 N까지 만들어지는 수
+		
+        // dp[k] = min { dp[k/3]+1 (if x%3==0)
+        //             { dp[k/2]+1 (if x%2==0)
+        //             { dp[k-1]+1
+        
+		dp[1] = 0;
+		for (int i=2;i<=N;i++) {
+			dp[i] = dp[i-1]+1;
+			if (i%2==0) dp[i] = Math.min(dp[i], dp[i/2]+1);
+			if (i%3==0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+		}
+		System.out.println(dp[N]);
+	}
 }
