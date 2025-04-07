08a860fa583899c6c7002c6aa150f5b6de8c1711
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-04-07 10:40:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 112 ms, Memory: 12192 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -22,38 +22,21 @@ public class Main {
 	// 최대 K 무게를 넘기지 않는 선에서 V(가치)의 총합이 최대가 되는 경우 찾기
 	
 	public static void main(String[] args) throws IOException{
-		N = readInt(); // 물품 수
-		K = readInt(); // 버틸 수 있는 무게, K
+		int N = readInt(); // 물품 수
+		int K = readInt(); // 버틸 수 있는 무게, K
 		
-		product = new Edge[N];
+		Edge[] product = new Edge[N];
 		for (int i=0;i<N;i++) product[i] = new Edge(readInt(), readInt()); // 물건별 무게, 가치 입력
 		
-		dp = new int[N+1][K+1]; // 어떤 물건들을 조합해서 만들 수 있는 무게 별 가치 최대 값 저장
-		for (int i=0;i<=N;i++) Arrays.fill(dp[i], -1);
-		System.out.println(recursive(0, 0));
-	}
-	
-	static Edge[] product;
-	static int N, K, dp[][];
-	
-	private static int recursive(int idx, int weightSum) {
-		
-		if (idx==N) return 0;
-		
-		if (dp[idx][weightSum]!=-1) {
-			return dp[idx][weightSum];
+		int[] dp = new int[K+1]; // 어떤 물건들을 조합해서 만들 수 있는 무게 별 가치 최대 값 저장
+		for (int i=0;i<N;i++) {
+			int v = product[i].value;
+			int w = product[i].weight;
+			
+			for (int weight=K; weight-w>=0;weight--) {
+				dp[weight] = Math.max(dp[weight], dp[weight-w]+v);
+			}
 		}
-		
-		// idx번째 물건 선택 X
-		int result = recursive(idx+1, weightSum);
-		
-		// idx번째 물건 선택 O
-		int v = product[idx].value;
-		int w = product[idx].weight;
-		if (weightSum+w<=K) {
-			result = Math.max(result, recursive(idx+1, weightSum+w)+v);
-		}
-		
-		return dp[idx][weightSum] = result;
+		System.out.println(dp[K]);
 	}
 }
