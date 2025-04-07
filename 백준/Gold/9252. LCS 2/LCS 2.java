40297ba0691be451dfcd75c4a0ad3f00aa21fe1e
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-04-07 16:36:51 +0900

    [Gold IV] Title: LCS 2, Time: 124 ms, Memory: 18288 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,43 +1,38 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String A = br.readLine();
-		String B = br.readLine();
+		char[] A = br.readLine().toCharArray();
+		char[] B = br.readLine().toCharArray();
 		
-		int lenA = A.length();
-		int lenB = B.length();
+		int lenA = A.length;
+		int lenB = B.length;
 		int[][] dp = new int[lenA+1][lenB+1];
 		
 		// LCS 구하기
 		for (int i=1;i<=lenA;i++) {
 			for (int j=1;j<=lenB;j++) {
-				char a = A.charAt(i-1);
-				char b = B.charAt(j-1);
-				if (a==b) dp[i][j] = dp[i-1][j-1]+1;
+				if (A[i-1]==B[j-1]) dp[i][j] = dp[i-1][j-1]+1;
 				else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
 			}
 		}
 		
 		// 문자열 복구하기
 		StringBuilder sb = new StringBuilder();
-		int i = lenA;
-		int j = lenB;
+		int i = lenA, j = lenB;
 		while(i>0 && j>0) {
-			char a = A.charAt(i-1);
-			char b = B.charAt(j-1);
-			if (a!=b) {
+			if (A[i-1]!=B[j-1]) {
 				if (dp[i-1][j]>dp[i][j-1]) i--;
 				else j--;
 			} else {
-				sb.append(a);
+				sb.append(A[i-1]);
 				i--;
 				j--;
 			}
 		}
-		System.out.printf("%s\n%s", dp[lenA][lenB], sb.reverse());
+		System.out.println(dp[lenA][lenB]);
+		System.out.println(sb.reverse());
  	}
 }
