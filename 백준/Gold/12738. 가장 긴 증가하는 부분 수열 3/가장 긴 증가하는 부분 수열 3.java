b0eb15901def3da0dfcca6f56fb33fb484269d23
hyewon <116737301+0w0n2@users.noreply.github.com>
2025-04-10 04:58:45 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 456 ms, Memory: 26104 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+	
+	private static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+	private static int readInt() throws IOException {
+		st.nextToken();
+		return (int) st.nval;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		int N = readInt(); // 수열 A의 크기
+		int[] num = new int[N];
+		
+		for (int i=0;i<N;i++) num[i] = readInt(); // 수열 A를 이루고 있는 Ai들..(수퍼노바)
+		
+		StringBuilder sb = new StringBuilder();
+		System.out.print(LIS(N, num));
+	}
+	
+	static int[] dp;
+	
+	private static int LIS(int N, int[] num) {
+		// LIS를 구하는데, DP+이분탐색으로 시간복잡도 줄이기
+		dp = new int[N+1]; // 어떤 len (길이)를 갖는 부분 수열 중에 마지막 값이 최소값인 애들의 마지막값을 담음
+		
+		int len = 0;
+		for (int i=0;i<N;i++) {
+			int idx = binarySearch(1, len, num[i]);
+			dp[idx] = num[i];
+			
+			if (idx>len) len = idx;
+		}
+		
+		return len;
+	}
+	
+	// dp[0]~dp[len] 구간에서 num[i] 보다 작은 마지막값을 가지면서 가장 긴 len을 찾음
+	private static int binarySearch(int left, int right, int key) {
+		while(left<=right) {
+			int mid = (left+right)/2;
+			if (dp[mid]<key) { // 더 긴 len을 찾아보자
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return left;
+	}
+}
