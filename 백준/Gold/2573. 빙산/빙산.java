941f41a1e157811ca42bd448fff740626954166e
hyewon <dahxtq1@naver.com>
2025-06-11 00:53:01 +0900

    [Gold IV] Title: 빙산, Time: 380 ms, Memory: 123304 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -2,100 +2,101 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    private static int N, M;
+    private static boolean[][] isIceberg, isVisited;
+    private static final int[] dx = {0, 0, -1, 1};
+    private static final int[] dy = {-1, 1, 0, 0};
+
+    private static class Cor{
+        int x, y, height;
+        Cor(int x, int y, int height){
+            this.x = x;
+            this.y = y;
+            this.height = height;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         N = readInt();
         M = readInt();
 
-        Queue<Cor> q = new ArrayDeque<>();
+        Queue<Cor> icebergQueue = new ArrayDeque<>();
 
-        for (int i=0;i<N;i++){
-            for (int j=0;j<M;j++) {
-                int height = readInt();
-                if (height>0){
-                    q.offer(new Cor(i, j, height));
-                }
-            }
+        for (int i=0;i<N;i++) for (int j=0;j<M;j++) {
+            int height = readInt();
+            if (height>0) icebergQueue.offer(new Cor(i, j, height));
         }
 
         int year = 0;
-        while(!q.isEmpty()){
+        while(!icebergQueue.isEmpty()){
+
             isIceberg = new boolean[N][M];
-            for (Cor c : q) {
-                isIceberg[c.x][c.y] = true;
+            for (Cor c : icebergQueue) isIceberg[c.x][c.y] = true;
+
+            if (isSeparated(icebergQueue)){
+                System.out.print(year);
+                return;
             }
-
-            int ct = 0;
-            isVisited = new boolean[N][M];
-            for (Cor c : q){
-                if (isVisited[c.x][c.y]) continue;
-                fillArea(c.x, c.y);
-                ct++;
-                if (ct>=2) {
-                    System.out.print(year);
-                    return;
-                }
-            }
-
-            int size = q.size();
-
-            while(size-->0){
-                Cor c = q.poll();
-                int zeroCt = 0;
-                for (int k=0;k<4;k++){
-                    int nx = c.x + dx[k];
-                    int ny = c.y + dy[k];
-                    if (nx<0||nx>=N||ny<0||ny>=M||isIceberg[nx][ny]) continue;
-                    zeroCt++;
-                }
-
-                int nh = Math.max(c.height-zeroCt, 0);
-                if (nh>0) {
-                    q.offer(new Cor(c.x, c.y, nh));
-                }
-                // else isIceberg[c.x][c.y] = false;
-            }
-
+            simulation(icebergQueue);
             year++;
         }
-
         System.out.print(0);
     }
+    
+    // 녹이기
+    private static void simulation(Queue<Cor> icebergQueue){
+        int size = icebergQueue.size();
 
-    private static int[] dx = {0, 0, -1, 1};
-    private static int[] dy = {-1, 1, 0, 0};
+        while(size-->0){
+            Cor c = icebergQueue.poll();
+            int water = 0;
+            for (int idx=0; idx<4; idx++){
+                int nx = c.x + dx[idx];
+                int ny = c.y + dy[idx];
+                if (nx<0||nx>=N||ny<0||ny>=M||isIceberg[nx][ny]) continue;
+                water++;
+            }
 
-    private static void fillArea(int i, int j){
+            int newHeight = Math.max(c.height-water, 0);
+            if (newHeight>0) {
+                icebergQueue.offer(new Cor(c.x, c.y, newHeight));
+            }
+        }
+    }
+
+    // 두 덩어리 이상으로 분리되었는지 확인
+    private static boolean isSeparated(Queue<Cor> icebergQueue){
+
+        isVisited = new boolean[N][M];
+        int ct = 0;
+
+        for (Cor c : icebergQueue) {
+            if (!isVisited[c.x][c.y]) {
+                bfs(c.x, c.y);
+                if (++ct>=2) return true;
+            }
+        }
+        return false;
+    }
+    
+    // 영역 구하기
+    private static void bfs(int x, int y){
         Queue<Cor> q = new ArrayDeque<>();
-        q.offer(new Cor(i, j));
-        isVisited[i][j] = true;
+        q.offer(new Cor(x, y, 0));
+        isVisited[x][y] = true;
 
         while(!q.isEmpty()){
             Cor c = q.poll();
-            for (int k=0;k<4;k++){
-                int ni = c.x + dx[k];
-                int nj = c.y + dy[k];
-                if (ni<0||ni>=N||nj<0||nj>=M||isVisited[ni][nj]||!isIceberg[ni][nj]) continue;
-                isVisited[ni][nj] = true;
-                q.offer(new Cor(ni, nj));
+            for (int idx=0; idx<4; idx++){
+                int nx = c.x + dx[idx];
+                int ny = c.y + dy[idx];
+                if (nx<0||nx>=N||ny<0||ny>=M) continue;
+                if (isVisited[nx][ny]||!isIceberg[nx][ny]) continue;
+                isVisited[nx][ny] = true;
+                q.offer(new Cor(nx, ny, 0));
             }
         }
     }
-
-    private static int N, M;
-    private static boolean isIceberg[][], isVisited[][];
-    private static class Cor{
-        int x, y, height;
-        Cor(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-        Cor(int x, int y, int height){
-            this(x, y);
-            this.height = height;
-        }
-    }
-
 
     private static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
     private static int readInt() throws IOException{
