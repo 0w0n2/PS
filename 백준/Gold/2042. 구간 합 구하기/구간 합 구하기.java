a0d85935b0ac27d966b8bf49680adbc7518c7838
hyewon <dahxtq1@naver.com>
2025-06-17 22:31:30 +0900

    [Gold I] Title: 구간 합 구하기, Time: 500 ms, Memory: 109824 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,115 +1,88 @@
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static long[] arr, tree;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static class SegTree{
+        int size, start;
+        long[] arr;
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+        SegTree(int n){
+            size = n;
+            start = 1;
+            // start : 리프 노드(사이즈 1인 노드)가 시작하는 인덱스로, start 는 2의 거듭제곱 중 size 이상인 최소값이 되어야 한다. (2^start >= size)
+            // size : 전체 트리 크기 = 2*start (완전이진트리 구조 / [1, start*2 -1])
+            while (start < size) start *= 2;        // 리프노드 인덱스 구하기(start)
+            arr = new long[start * 2];         // 전체 트리 배열(size = start*2)
+        }
 
-		arr = new long[N + 1];
-		for (int i = 1; i <= N; i++) {
-			arr[i] = Long.parseLong(br.readLine());
-		}
+        // 리프 노드들의 값을 미리 준 후(st.arr[st.start + i] = readLong();), 해당 값들로 세그먼트를 구축(construct())
+        // 아래에서부터 위로 계산
+        // [1, start-1] 구간에 리프 노드가 존재할 때, 그 바로 위 노드들부터 인덱스가 감소하는 순으로 배열의 값을 구하는 것
+        void construct(){
+            for (int i=start-1; i>0; i--) arr[i] = arr[i*2] + arr[i*2+1];
+            // [i*2], [i*2+1]이 자식 노드이므로, 그 합을 부모 노드인 [i]에 저장
+        }
 
-		// 2^k >= N인 최소의 k를 찾아야 함.
-		// 양변에 log을 취하면,
-		// k >= logN / log2
-		// (logN / log2)의 값을 올림한 후 1을 더하면 k가 됨.
-		// 위에서 구한 k를 제곱하면 세그먼트 트리의 size를 구할 수 있음.
+        // i: 실제 배열 인덱스(세그먼트 트리에서는 start+i)
+        // 해당 리프 노드를 val 로 갱신하고, 그 변화량 (delta) 만틈 부모로 타고 올라가서 전체 노드 값을 반영
+        void update(int i, long val){
+            i += start;    // 세그먼트 트리에서의 인덱스
+            val -= arr[i];
+            // 그냥 arr[idx] = val 해버리면 부모 노드들은 얼마만큼 변화됐는지 모르니까
+            // 차이값 delta = val - arr[idx] 를 구해서 상위 노드들에 더해주는 방식
+            while (i > 0) { // 루트 노드까지
+                arr[i] += val;    // 변화량을 더해줌
+                i /= 2;
+            }
+        }
 
-//		int k = (int) Math.ceil(Math.log(N) / Math.log(2)) + 1;
-//		int size = (int) Math.pow(2, k);
-//		
-//		tree = new long[size];
+        public long sum(int a, int b) {
+            return sum(a, b, 1, 0, start);
+        }
 
-		// 사이즈를 구하는 위의 과정이 귀찮으면, 단순히 N에 4를 곱한 사이즈를 사용해도 무방함.
-		tree = new long[N * 4];
+        // a, b : 구하려는 구간 범위
+        // node : 현재 보고 있는 노드 번호
+        // na, nb : 현재 보고 있는 구간의 시작 인덱스, 끝 인덱스
+        private long sum(int a, int b, int node, int na, int nb){
+            if (b<=na || nb<=a) return 0;           // 범위 밖에 있는 경우
+            if (a<=na && nb<=b) return arr[node];   // 범위 안에 완전히 포함 되는 경우
 
-		init(1, N, 1);
+            // 그렇지 않다면 현재 노드의 구간을 두 부분으로 나누어 합을 구하기
+            // 구간을 2등분하면 왼쪽 것은 이 노드의 왼쪽 자식, 오른쪽 것은 오른쪽 자식이 표현하고 있음
+            //    ㅇ ㅇ
+            //  ㅇ ㄴ ㄴ ㅇ
+            int mid = (na+nb)/2;
+            return sum(a, b, node*2, na, mid) + sum(a, b, node*2 + 1, mid, nb);
+        }
+    }
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < M + K; i++) {
-			st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
 
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			long c = Long.parseLong(st.nextToken());
+        int N = Integer.parseInt(stk.nextToken()); // N개의 수
+        int M = Integer.parseInt(stk.nextToken()); // 수의 변경이 일어나는 횟수
+        int K = Integer.parseInt(stk.nextToken()); // 구간의 합을 구하는 횟수
+        SegTree st = new SegTree(N);
 
-			if (a == 1) {
-				long dif = c - arr[b];
-				arr[b] = c;
-				update(1, N, 1, b, dif);
-			} else if (a == 2) {
-				sb.append(sum(1, N, 1, b, (int) c) + "\n");
-			}
-		}
+        for (int i=0;i<N;i++) st.arr[st.start + i] = Long.parseLong(br.readLine()); // 리프 노드들의 값을 미리 줌
+        st.construct(); // 미리 준 리프 노드 값으로 윗 줄부터 루트까지 전체 세그먼트 트리 구축
 
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
-	// start: 시작 인덱스, end: 끝 인덱스
-	public static long init(int start, int end, int node) {
-		if (start == end) {
-			return tree[node] = arr[start];
-		}
-
-		int mid = (start + end) / 2;
-
-		// 재귀적으로 두 부분으로 나눈 뒤에 그 합을 자기 자신으로 함.
-		return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
-	}
-
-	// start: 시작 인덱스, end: 끝 인덱스
-	// left, right: 구간 합을 구하고자 하는 범위
-	public static long sum(int start, int end, int node, int left, int right) {
-		// 범위 밖에 있는 경우
-		if (left > end || right < start) {
-			return 0;
-		}
-
-		// 범위 안에 있는 경우
-		if (left <= start && end <= right) {
-			return tree[node];
-		}
-
-		// 그렇지 않다면, 두 부분으로 나누어 합을 구하기
-		int mid = (start + end) / 2;
-		return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
-	}
-
-	// start: 시작 인덱스, end: 끝 인덱스
-	// idx: 구간 합을 수정하고자 하는 노드
-	// dif: 수정할 값
-	public static void update(int start, int end, int node, int idx, long dif) {
-		// 범위 밖에 있는 경우
-		if (idx < start || idx > end) {
-			return;
-		}
-
-		// 범위 안에 있으면 내려가며 다른 원소도 갱신
-		tree[node] += dif;
-		if (start == end) {
-			return;
-		}
-
-		int mid = (start + end) / 2;
-		update(start, mid, node * 2, idx, dif);
-		update(mid + 1, end, node * 2 + 1, idx, dif);
-	}
-
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<M+K;i++){
+            stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            if (a==1){
+                int b = Integer.parseInt(stk.nextToken());
+                long c = Long.parseLong(stk.nextToken());
+                st.update(b-1, c);
+            } else {
+                int b = Integer.parseInt(stk.nextToken());
+                int c = Integer.parseInt(stk.nextToken());
+                sb.append(st.sum(b-1, c)).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
 }
