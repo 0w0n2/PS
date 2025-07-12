e728b1f52e44c7df125a5d075f41dc3ba1a3ec71
hyewon <dahxtq1@naver.com>
2025-07-12 19:54:43 +0900

    [Gold IV] Title: 뒤집기 3, Time: 60 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Gold/1464. 뒤집기 3/뒤집기 3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        ArrayDeque<Character> q = new ArrayDeque<>();
+        q.offer(S.charAt(0));
+        for (int i=1;i<S.length();i++){
+            char s = S.charAt(i);
+            if (s <= q.getFirst()) q.offerFirst(s);
+            else q.offerLast(s);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char s : q) sb.append(s);
+        System.out.print(sb);
+    }
+
+}
