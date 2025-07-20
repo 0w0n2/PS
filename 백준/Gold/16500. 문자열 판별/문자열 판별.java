bbda96f9341ba97442972872ea65102e823e8b0a
hyewon <dahxtq1@naver.com>
2025-07-20 20:06:07 +0900

    [Gold V] Title: 문자열 판별, Time: 68 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Gold/16500. 문자열 판별/문자열 판별.java
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.java
@@ -4,26 +4,28 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException{
-        char[] S = readString().toCharArray();
-        HashSet<String> A = new HashSet<>();
+        S = readString();
         int N = readInt();
         for (int i=0;i<N;i++) A.add(readString());
-        System.out.print(sol(S, A));
+        System.out.print(S.length()==0 ? 0 : sol());
     }
 
-    private static int sol(char[] S, HashSet<String> A) throws IOException{
-        int sLen = S.length;
+    private static String S;
+    private static HashSet<String> A = new HashSet<>();
+
+    private static int sol() throws IOException{
+        int sLen = S.length();
         int[] dp = new int[sLen+1];
         dp[0] = 1;
 
         for (int i=0;i<sLen;i++){ // 검사 시작지점
             if (dp[i]==0) continue; // 이미 판별된 구간은 뛰어넘기
-            for (int j=i+1;j<=sLen;j++){ // S[j:k] 슬라이싱
-                StringBuilder sb = new StringBuilder();
-                for (int k=i;k<j;k++) sb.append(S[k]);
-                if (A.contains(sb.toString())){
-                    dp[j] = 1;
-                    if (j==sLen) return 1;
+            for (String a : A){
+                int end = i + a.length();
+                if (end>sLen) continue;
+                if (S.startsWith(a, i)){
+                    dp[end] = 1;
+                    if (end==sLen) return 1;
                 }
             }
         }
