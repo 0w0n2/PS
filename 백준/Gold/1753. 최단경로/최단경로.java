437b892a0dcd12fab23ca942671c34f7d70270f2
hyewon <dahxtq1@naver.com>
2025-08-31 18:19:00 +0900

    [Gold IV] Title: 최단경로, Time: 348 ms, Memory: 24824 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,60 +2,83 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+	
 	private static final StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-	private static int readInt() throws IOException {
+	private static int readInt() throws IOException{
 		st.nextToken();
 		return (int) st.nval;
 	}
 	
-	public static void main(String[] args) throws IOException {
-		V = readInt(); // 정점의 개수
-		int E = readInt(); // 간선의 개수
-		int S = readInt()-1;
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i=0;i<V;i++) map.add(new ArrayList<int[]>());
-		
-		for (int i=0;i<E;i++) {
-			int s = readInt()-1;
-			int e = readInt()-1;
-			int w = readInt();
-			map.get(s).add(new int[] {e, w});
-		}
-		
-		dijkstra(S);
-		for (int i : dist) sb.append(i==INF ? "INF" : i).append("\n");
-		System.out.print(sb);
+	static int V, E, start;
+	static int[] head, to, weight, next;
+	static int edgePtr = 0;
+	
+	static ArrayList<ArrayList<Integer>> map = new ArrayList<ArrayList<Integer>>();
+	
+	static void addEdge(int u, int v, int w) {
+		to[edgePtr] = v;
+		weight[edgePtr] = w;
+		next[edgePtr] = head[u];
+		head[u] = edgePtr++;
 	}
 	
-	private static int V;
-	private static ArrayList<ArrayList<int[]>> map = new ArrayList<ArrayList<int[]>>();
-	private static int[] dist;
-	private final static int INF = 1_000_000_000;;
-	
-	private static void dijkstra(int s) {
-		PriorityQueue<Long> pq = new PriorityQueue<Long>();
+	public static void main(String[] args) throws IOException{
+		V = readInt(); // 정점의 개수
+		E = readInt(); // 간선의 개수
+		start = readInt(); // 시작 지점
 		
-		dist = new int[V];
+		head = new int[V+1];
+		Arrays.fill(head, -1);
+		to = new int[E];
+		weight = new int[E];
+		next = new int[E];
+		
+		for (int i=0;i<E;i++) {
+			int u = readInt();
+			int v = readInt();
+			int w = readInt();
+			addEdge(u, v, w);
+		}
+		
+		final int INF = 1_000_000_000;
+		int[] dist = new int[V+1];
 		Arrays.fill(dist, INF);
-		dist[s] = 0;
-		pq.offer(((long) dist[s] << 32) | s);
+		
+		// long 하나에 두 개의 int 정보 담기
+		// long : 64비트 / int : 32비트
+		// 64비트짜리 long 변수 공간을 반으로 나누어 앞 32비트에는 "거리", 뒤 32비트에는 "정점 번호"를 저장
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		dist[start] = 0;
+		pq.offer(((long) dist[start]) << 32 | start);
+		// `dist[start]` 의 비트를 왼쪽으로 32칸 밀어냄 
+		// `| start` : OR 연산자 (왼쪽 시프트 연산 결과로 하위 32비트가 모두 0이 되었으므로 start 정점 번호를 OR 연산하면 하위 32비트에 start의 값이 그대로 채워짐)
 		
 		while (!pq.isEmpty()) {
 			long cur = pq.poll();
-			int u = (int) (cur & 0xFFFFFFFL);
+			// 정보 분리하기
+			int u = (int) (cur & 0xFFFFFFFFL);
+			// 0xFFFFFFFFL : 하위 32비트가 모두 1인 값 + Long 타입임을 명시
+			// & 연산하면 하위 32비트에 해당하는 값만 분리됨
 			int d = (int) (cur >>> 32);
-			if (dist[u] != d) continue;
-
-			for (int[] next : map.get(u)) {
-				int nw = d + next[1];
-				if (dist[next[0]] > nw) {
-					dist[next[0]] = nw;
-					pq.offer(((long) nw << 32) | next[0]);
+			// >> 연산자 : 그냥 0 채우면서 shift
+			// >>> 연산자 : 부호를 고려하면서 shift
+			
+			if (d!=dist[u]) continue;
+			
+			for (int e = head[u]; e!=-1; e=next[e]) {
+				int v = to[e];
+				int nd = d + weight[e];
+				if (nd < dist[v]) {
+					dist[v] = nd;
+					pq.offer(((long) nd) << 32 | v);
 				}
-			} 
+			}
 		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i=1;i<=V;i++) {
+			sb.append(dist[i] == INF ? "INF" : dist[i]).append("\n");
+		}
+		System.out.print(sb);
 	}
 }
