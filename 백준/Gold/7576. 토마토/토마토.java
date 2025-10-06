8139819460d49d9ded59831f4e7865b5d5cdb8a9
hyewon <dahxtq1@naver.com>
2025-10-07 00:51:20 +0900

    [Gold V] Title: 토마토, Time: 324 ms, Memory: 47488 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,70 +2,62 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m;
-    static int [][] map;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());   // 가로
-        n = Integer.parseInt(st.nextToken());   // 세로
-        map = new int [n][m];
-        boolean tomato = false;
-        Queue<int[]> q = new LinkedList<>();
-
-        for (int i=0;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            for (int j=0;j<m;j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j]==0) tomato = true;
-                if (map[i][j]==1) q.offer(new int[] {i, j});
-            }
-        }
-        if (!tomato){
-            System.out.println(0);
-            return;
-        }
-
-        System.out.println(bfs(q));
-    }
-
-    public static int bfs(Queue<int[]> q) {
-        
-        int [] dx = {0, 0, 1, -1};
-        int [] dy = {1, -1, 0, 0};
-        
-        int days = 0;
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] xy = q.poll();
-                int x = xy[0];
-                int y = xy[1];
-
-                for (int k = 0; k < 4; k++) {
-                    int nx = x + dx[k];
-                    int ny = y + dy[k];
-
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                    
-                    if (map[nx][ny] == 0) {
-                        map[nx][ny] = 1; 
-                        q.offer(new int[]{nx, ny});
-                    }
-                }
-            }
-            days++;
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == 0) {
-                    return -1;
-                }
-            }
-        }
-        return days - 1;
-    }
+	
+	private static Queue<int[]> q = new ArrayDeque<>();
+	private static int map[][], N, M, unripeCt = 0;
+	
+	public static void main(String[] args) throws IOException{
+		
+		M = readInt();
+		N = readInt();
+		
+		map = new int[N][M];
+		
+		for (int n=0;n<N;n++) {
+			for (int m=0;m<M;m++) {
+				map[n][m] = readInt();
+				if (map[n][m] == 1) {
+					q.offer(new int[] {n, m});
+				} else if (map[n][m] == 0) {
+					unripeCt++;
+				}
+			}
+		}
+		
+		System.out.print((unripeCt == 0) ? 0 : simulate());
+	}
+	
+	private static int simulate() {	
+		
+		final int[] dx = new int[] {-1, 1, 0, 0};
+		final int[] dy = new int[] {0, 0, -1, 1};
+		int day = -1;
+		
+		while(!q.isEmpty()) {
+			day++;
+			int size = q.size();
+			while(size-- > 0) {
+				int[] c = q.poll();
+				
+				for (int i=0;i<4;i++) {
+					int nx = c[0] + dx[i];
+					int ny = c[1] + dy[i];
+					
+					if (nx<0 || nx>=N || ny<0 || ny>=M) continue;
+					if (map[nx][ny]==0) {
+						map[nx][ny] = 1;
+						q.offer(new int[] {nx, ny});
+						unripeCt--;
+					}
+				}
+			}
+		}
+		return unripeCt == 0 ? day : -1;
+	}
+	
+	private static final StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+	private static int readInt() throws IOException{
+		st.nextToken();
+		return (int) st.nval;
+	}
 }
