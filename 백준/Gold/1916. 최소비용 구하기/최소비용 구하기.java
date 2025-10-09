d80345651ead3be4a45fff4a9fb3ae79c82c0705
hyewon <dahxtq1@naver.com>
2025-10-09 22:09:45 +0900

    [Gold V] Title: 최소비용 구하기, Time: 452 ms, Memory: 54444 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,64 +2,59 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class Node implements Comparable<Node>{
-        int v, cost;
-        Node(int v, int cost){
-            this.v = v;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.cost, o.cost);
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        int N = readInt();
-        int M = readInt();
-
-        ArrayList<ArrayList<Node>> map = new ArrayList<>();
-        for (int i=0;i<=N;i++) map.add(new ArrayList<>());
-
-        for (int i=0;i<M;i++){
-            int v = readInt();
-            int e = readInt();
-            int w = readInt();
-            map.get(v).add(new Node(e, w));
-        }
-
-        int start = readInt();
-        int end = readInt();
-        System.out.print(dijkstra(N, start, end, map));
-    }
-
-    private static int dijkstra(int N, int start, int end, ArrayList<ArrayList<Node>> map){
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-
-        while(!pq.isEmpty()){
-            Node c = pq.poll();
-            if (dist[c.v] < c.cost) continue;
-
-            for (Node nextPoint:map.get(c.v)){
-                int nc = c.cost + nextPoint.cost;
-                if (dist[nextPoint.v]>nc){
-                    dist[nextPoint.v] = nc;
-                    pq.offer(new Node(nextPoint.v, nc));
-                }
-            }
-        }
-        return dist[end];
-    }
-
-    private static final StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-    private static int readInt() throws IOException{
-        st.nextToken();
-        return (int) st.nval;
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		map = new ArrayList<ArrayList<int[]>>();
+		for (int i=0;i<=N;i++) map.add(new ArrayList<int[]>());
+		
+		StringTokenizer st;
+		for (int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			
+			map.get(u).add(new int[] {v, w});
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+		
+		dijkstra(start, end);
+	}
+	
+	private static ArrayList<ArrayList<int[]>> map;
+	private static int N;
+	
+	private static void dijkstra(int start, int end) {
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[1]-b[1]);
+		pq.offer(new int[] {start, 0});
+		
+		int[] dist = new int[N+1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[start] = 0;
+		
+		while(!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			
+			if (cur[0] == end) {
+				System.out.print(cur[1]);
+				return;
+			}
+			
+			if (dist[cur[0]] != cur[1]) continue;
+			
+			for (int[] next : map.get(cur[0])) {
+				int nw = next[1] + cur[1];
+				if (dist[next[0]] > nw) {
+					dist[next[0]] = nw;
+					pq.offer(new int[] {next[0], nw});
+				}
+			}
+		}
+	}
 }
