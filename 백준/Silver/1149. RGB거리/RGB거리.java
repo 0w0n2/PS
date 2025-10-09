11205d2c58382bcd2307e906d9776a7f619eb009
hyewon <dahxtq1@naver.com>
2025-10-09 21:55:23 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 12668 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,27 +1,41 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 집 개수
-        int [][] costs = new int[n+1][3]; // 집 칠하는 비용
-        int [][] dp = new int[1001][3];
-        dp[0][0] = dp[0][1] = dp[0][2] = 0;
-
-        for (int i=1; i<=n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            costs[i][0] = Integer.parseInt(st.nextToken());
-            costs[i][1] = Integer.parseInt(st.nextToken());
-            costs[i][2] = Integer.parseInt(st.nextToken());
-        }
-        for (int i=1; i<=n; i++){
-            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + costs[i][0]; //앞이 1이거나 2
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + costs[i][1]; //앞이 0이거나 2
-            dp[i][2] = Math.min(dp[i-1][1], dp[i-1][0]) + costs[i][2]; // 앞이 1이거나 0
-        }
-        System.out.print(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
-    }
+	private static final StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+	private static int readInt() throws IOException{
+		st.nextToken();
+		return (int) st.nval;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		N = readInt();
+		costs = new int[N][3];
+		
+		int[][] dp = new int[N][3];
+		for (int i=0;i<N;i++) {
+			for (int j=0;j<3;j++) {
+				costs[i][j] = readInt();
+			}
+		}
+		
+		for (int i=0;i<3;i++) {
+			dp[0][i] = costs[0][i];
+		}
+		
+		for (int i=1;i<N;i++) {
+			for (int j=0;j<3;j++) {
+				dp[i][j] = Math.min(dp[i-1][(j+1)%3], dp[i-1][(j+2)%3]) + costs[i][j];
+			}
+		}
+		
+		int result = Integer.MAX_VALUE;
+		for (int i=0;i<3;i++) {
+			result = Math.min(dp[N-1][i], result);
+		}
+		System.out.print(result);
+	}
+	
+	private static int N, costs[][];
 
 }
