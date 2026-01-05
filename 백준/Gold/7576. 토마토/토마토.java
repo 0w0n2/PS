6644401cc4d894c270445a193747be5927e9d6fe
hyewon <dahxtq1@naver.com>
2026-01-06 01:54:56 +0900

    [Gold V] Title: 토마토, Time: 312 ms, Memory: 46596 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,62 +2,57 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	private static Queue<int[]> q = new ArrayDeque<>();
-	private static int map[][], N, M, unripeCt = 0;
-	
-	public static void main(String[] args) throws IOException{
-		
-		M = readInt();
-		N = readInt();
-		
-		map = new int[N][M];
-		
-		for (int n=0;n<N;n++) {
-			for (int m=0;m<M;m++) {
-				map[n][m] = readInt();
-				if (map[n][m] == 1) {
-					q.offer(new int[] {n, m});
-				} else if (map[n][m] == 0) {
-					unripeCt++;
-				}
-			}
-		}
-		
-		System.out.print((unripeCt == 0) ? 0 : simulate());
-	}
-	
-	private static int simulate() {	
-		
-		final int[] dx = new int[] {-1, 1, 0, 0};
-		final int[] dy = new int[] {0, 0, -1, 1};
-		int day = -1;
-		
-		while(!q.isEmpty()) {
-			day++;
-			int size = q.size();
-			while(size-- > 0) {
-				int[] c = q.poll();
-				
-				for (int i=0;i<4;i++) {
-					int nx = c[0] + dx[i];
-					int ny = c[1] + dy[i];
-					
-					if (nx<0 || nx>=N || ny<0 || ny>=M) continue;
-					if (map[nx][ny]==0) {
-						map[nx][ny] = 1;
-						q.offer(new int[] {nx, ny});
-						unripeCt--;
-					}
-				}
-			}
-		}
-		return unripeCt == 0 ? day : -1;
-	}
-	
-	private static final StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-	private static int readInt() throws IOException{
-		st.nextToken();
-		return (int) st.nval;
-	}
+    private static final StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    private static int readInt() throws IOException{
+        st.nextToken();
+        return (int) st.nval;
+    }
+
+    public static void main(String[] args) throws IOException{
+        int M = readInt(); // 가로 길이 (열)
+        int N = readInt(); // 세로 길이 (행)
+
+        int[][] tomato = new int[N][M];
+        int yet = 0;
+        Queue<int[]> q = new ArrayDeque<>();
+
+        for (int i=0;i<N;i++) for (int j=0;j<M;j++) {
+            if ((tomato[i][j]=readInt())==0) {
+                yet++;
+            } else if (tomato[i][j]==1) {
+                q.offer(new int[] {i, j});
+            }
+        }
+
+        int[] dx = new int[]{0, 0, -1, 1};
+        int[] dy = new int[]{1, -1, 0, 0};
+
+        if (yet == 0) { // 조기 종료: 모든 토마토가 익어 있음
+            System.out.print(0);
+            return;
+        }
+
+
+        int day = 1;
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-->0) {
+                int[] cur = q.poll();
+                for (int k=0;k<4;k++) {
+                    int nx = cur[0] + dx[k];
+                    int ny = cur[1] + dy[k];
+                    if (nx<0||nx>=N||ny<0||ny>=M||tomato[nx][ny]!=0) continue;
+
+                    tomato[nx][ny] = 1;
+                    q.offer(new int[] {nx, ny});
+                    yet--;
+                }
+            }
+            if (yet==0) break;
+            day++;
+        }
+
+        System.out.print((yet>0) ? -1 : day);
+    }
 }
