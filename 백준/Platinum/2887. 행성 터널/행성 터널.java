7f87c73e398af179e908bfec6f91f979904beedb
Hyewon Lee <dahxtq1@naver.com>
2026-03-09 15:19:36 +0900

    [Platinum V] Title: 행성 터널, Time: 1924 ms, Memory: 71848 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -2,23 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-//	static class Planet{
-//		int num, x, y, z; // 행성 번호, x-y-z 좌표
-//		public Planet(int num, int x, int y, int z) {
-//			this.num = num;
-//			this.x = x;
-//			this.y = y;
-//			this.z = z;
-//		}
-//	}
-	
-	static class Planet{
-		int num, v; // 행성 번호, x-y-z 좌표
-		public Planet(int num, int v) {
-			this.num = num;
-			this.v = v;
-		}
-	}
 	
 	static class Node{
 		int start, end, dist;
@@ -29,8 +12,8 @@ public class Main {
 		}
 	}
 	
-	static ArrayList<Planet> coordinate = new ArrayList<>();
 	static int[] par;
+	static int[] height;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,37 +21,39 @@ public class Main {
 		
 		par = new int[n];
 		Arrays.fill(par, -1);
-		
-		ArrayList<Planet> X = new ArrayList<>();
-		ArrayList<Planet> Y = new ArrayList<>();
-		ArrayList<Planet> Z = new ArrayList<>();
+		height = new int[n];
+		Arrays.fill(height, 1);
+
+		int[][] coordinate = new int[n][4]; // 0:행성 번호, 1:x, 2:y, 3:z
 		
 		StringTokenizer st;
 		for (int i=0;i<n;i++) {
+			coordinate[i][0] = i; // 행성 번호
 			st = new StringTokenizer(br.readLine());
-			X.add(new Planet(i, Integer.parseInt(st.nextToken())));
-			Y.add(new Planet(i, Integer.parseInt(st.nextToken())));
-			Z.add(new Planet(i, Integer.parseInt(st.nextToken())));
+			for (int j=1;j<4;j++) coordinate[i][j] = Integer.parseInt(st.nextToken()); // 각 x,y,z 좌표
 		}
 		
-		Collections.sort(X, (o1, o2)-> o1.v - o2.v); // 인접 좌표끼리 이어지게
-		Collections.sort(Y, (o1, o2)-> o1.v - o2.v); // 인접 좌표끼리 이어지게
-		Collections.sort(Z, (o1, o2)-> o1.v - o2.v); // 인접 좌표끼리 이어지게
-		
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2)-> o1.dist-o2.dist); // dist(거리 가중치) 짧은 순으로 큐 정렬 
-		
-		for (int i=1;i<n;i++) {
-			pq.add(new Node(X.get(i-1).num, X.get(i).num, Math.abs(X.get(i-1).v-X.get(i).v)));
-			pq.add(new Node(Y.get(i-1).num, Y.get(i).num, Math.abs(Y.get(i-1).v-Y.get(i).v)));
-			pq.add(new Node(Z.get(i-1).num, Z.get(i).num, Math.abs(Z.get(i-1).v-Z.get(i).v)));
+		List<Node> q = new ArrayList<>(); 
+		for (int i=1;i<4;i++) {
+			final int idx = i;
+			Arrays.sort(coordinate, (o1, o2) -> Integer.compare(o1[idx], o2[idx])); // 가능한 인접 좌표끼리 이어지게(두 행성의 좌표간의 차이가 적게) 정렬
+			for (int j=1; j<n; j++) {
+				q.add(new Node(coordinate[j-1][0], coordinate[j][0], Math.abs(coordinate[j-1][i]-coordinate[j][i])));
+			}
 		}
 		
+		Collections.sort(q, (o1, o2) -> o1.dist - o2.dist); // dist(거리 가중치) 짧은 순으로 큐 정렬 
 		
-		while(!pq.isEmpty()) {
-			Node current = pq.poll();
-			merge(current.start, current.end, current.dist);
+		int step = 0;
+		int result = 0;
+		for (Node current : q) {
+			if (merge(current.start, current.end)) {
+				result += current.dist;
+				step ++;
+			}
+			if (step==n-1) break;
 		}
-		System.out.println(totalCost);
+		System.out.print(result);
 	}
 	
 	static int find(int k) {
@@ -76,15 +61,17 @@ public class Main {
 		return par[k] = find(par[k]);
 	}
 	
-	static int totalCost = 0;
-	
-	static void merge(int a, int b, int currentCost) {
+	static boolean merge(int a, int b) {
 		a = find(a);
 		b = find(b);
 		
-		if (a==b) return;
+		if (a==b) return false;
 		
-		par[a] = b;
-		totalCost += currentCost;
+		if (height[a]>height[b]) par[b] = a;
+		else {
+			par[a] = b;
+			if (height[a]==height[b]) height[b]++;
+		}
+		return true;
 	}
 }
