f6f0fbda3d0e4127b5b24764acdcaafadc9c5cb5
Hyewon Lee <dahxtq1@naver.com>
2026-07-04 20:28:15 +0900

    Time: 19 ms (90.5%), Space: 110.8 MB (89.55%) - LeetHub

--- LeetCode/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ LeetCode/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -4,42 +4,44 @@ using namespace std;
 
 class Solution {
 public:
-    struct Node {
-        int v, dist;
-    };
+    vector<int> parent;
 
-    int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<Node>> map(n+1);
-
-        for (const vector<int>& r : roads) {
-            int u = r[0];
-            int v = r[1];
-            int d = r[2];
-
-            map[u].push_back({v, d});
-            map[v].push_back({u, d});
+    int find(int x) {
+        if (parent[x] == -1) {
+            return x;
         }
 
+        return parent[x] = find(parent[x]);
+    }
+
+    void merge(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a==b) {
+            return;
+        }
+
+        parent[b] = a;
+    }
+
+    int minScore(int n, vector<vector<int>>& roads) {
+        parent.resize(n+1);
+
+        for (int i=1;i<=n;i++) {
+            parent[i] = -1;
+        }
+
+        for (const vector<int>& r : roads) {
+            merge(r[0], r[1]);
+        }
+
+        int root = find(1);
         int ans = INT_MAX;
-        vector<bool> isVisited(n+1, false);
-        isVisited[1] = true;
 
-        queue<int> q;
-        q.push(1);
-
-        // 갈 수 있는 모든 경로를 탐색하면서 최솟값 갱신
-        while (!q.empty()) {
-            int cur = q.front(); q.pop();
-
-            for (const Node& nextNode : map[cur]) {
-                ans = min(ans, nextNode.dist);
-
-                if (isVisited[nextNode.v]) {
-                    continue;
-                }
-
-                isVisited[nextNode.v] = true;
-                q.push(nextNode.v);
+        for (const vector<int>& r : roads) {
+            if (find(r[0]) == root || find(r[1]) == root) {
+                ans = min(ans, r[2]);
             }
         }
 
