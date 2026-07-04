c92c3b1f1d3707964831604d5e7a1c9cfa21f7c2
Hyewon Lee <dahxtq1@naver.com>
2026-07-04 20:41:02 +0900

    Time: 10 ms (91.45%), Space: 134.1 MB (90.13%) - LeetHub

--- LeetCode/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ LeetCode/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,40 +1,21 @@
 import java.util.*;
 
 class Solution {
-    class Node implements Comparable<Node>{
-        int u, v, dist;
-
-        Node(int[] r) {
-            this.u = r[0];
-            this.v = r[1];
-            this.dist = r[2];
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.dist, o.dist);
-        }
-    }
-
     int[] parent;
     public int minScore(int n, int[][] roads) {
         parent = new int[n+1];
         Arrays.fill(parent, -1);
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
         for (int[] r : roads) {
             merge(r[0], r[1]);
-            pq.offer(new Node(r));
         }
 
         int root = find(1);
         int ans = Integer.MAX_VALUE;
 
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
-            if (find(cur.u) == root || find(cur.v) == root) {
-                ans = cur.dist;
-                break;
+        for (int[] r : roads) {
+            if (find(r[0])==root || find(r[1])==root) {
+                ans = Math.min(ans, r[2]);
             }
         }
 
