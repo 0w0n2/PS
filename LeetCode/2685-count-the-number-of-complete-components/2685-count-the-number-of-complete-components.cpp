86336d67f6e48077f0ca2c7aea604fd1c202e8ba
Hyewon Lee <dahxtq1@naver.com>
2026-07-12 05:55:42 +0900

    Time: 7 ms (97.33%), Space: 117 MB (99.2%) - LeetHub

--- LeetCode/2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ LeetCode/2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -7,45 +7,52 @@ public:
     // 노드가 k개인 graph의 간선 수 = k*(k-1)/2
     // degree 합 = 간선 수 * 2
     // 순환 조건: degree 합 == k*(k-1)  
+    vector<int> par;
+
+    int find(int x) {
+        if (par[x] < 0) {
+            return x;
+        }
+        return par[x] = find(par[x]);
+    }
+
+    void merge(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a==b) {
+            return;
+        }
+
+        par[b] = a;
+    }
+
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-       vector<vector<int>> graph(n);
-       for (const vector<int>& edge : edges) {
-        int u = edge[0];
-        int v = edge[1];
+        par.assign(n, -1);
+        
+        for (const vector<int>& e : edges) {
+            merge(e[0], e[1]);
+        }
 
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-       } 
+        vector<int> nodeCt(n, 0);
+        vector<int> degreeCt(n, 0);
 
-       vector<bool> isVisited(n, false);
-       int ans = 0;
+        for (int i=0; i<n; i++) {
+            int root = find(i);
+            nodeCt[root]++;
+        }
 
-       for (int i=0; i<n; i++) {
-            if (isVisited[i]) continue;
+        for (const vector<int>& e : edges) {
+            int root = find(e[0]);
+            degreeCt[root] += 2;
+        }
 
-            int nodeCt = 0;
-            int degreeCt = 0;
+        int ans = 0;
+        for (int i=0; i<n; i++) {
+            if (nodeCt[i] == 0) continue;
+            if (degreeCt[i] == nodeCt[i] * (nodeCt[i]-1)) ans++;
+        }
 
-            queue<int> q;
-            q.push(i);
-            isVisited[i] = true;
-
-            while (!q.empty()) {
-                int cur = q.front(); q.pop();
-
-                nodeCt++;
-                degreeCt += graph[cur].size();
-
-                for (int next : graph[cur]) {
-                    if (isVisited[next]) continue;
-                    isVisited[next] = true;
-                    q.push(next);
-                }
-            }
-
-            if (degreeCt == nodeCt * (nodeCt - 1)) ans++;
-       }
-       
-       return ans;
+        return ans;
     }
 };
