72fe8e9326d35aad8d501a7510c6b7bdd3996878
Hyewon Lee <dahxtq1@naver.com>
2026-07-20 23:37:40 +0900

    [level 3] Title: 가장 먼 노드, Time: 14.32 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,32 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        List<Integer>[] map = new ArrayList[n+1];
-        for (int i=1;i<=n;i++) map[i] = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+        for (int i=0; i<=n; i++) map.add(new ArrayList<>());
         
         for (int[] e : edge) {
-            map[e[0]].add(e[1]);
-            map[e[1]].add(e[0]);
+            map.get(e[0]).add(e[1]);
+            map.get(e[1]).add(e[0]);
         }
         
         Queue<Integer> q = new ArrayDeque<>();
-        boolean[] isVisited = new boolean[n+1];
-        
         q.offer(1);
+        
+        boolean[] isVisited = new boolean[n+1];
         isVisited[1] = true;
         
         int size = 0;
-        
         while (!q.isEmpty()) {
             size = q.size();
             
-            for (int i=0;i<size;i++) {
-                int cur = q.poll();
+            for (int i=0; i<size; i++) {
+                int curNode = q.poll();
                 
-                for (int next : map[cur]) {
-                    if (!isVisited[next]) {
-                        isVisited[next] = true;
-                        q.offer(next);
+                for (int nextNode : map.get(curNode)) {
+                    if (!isVisited[nextNode]) {
+                        q.offer(nextNode);
+                        isVisited[nextNode] = true;
                     }
                 }
             }
