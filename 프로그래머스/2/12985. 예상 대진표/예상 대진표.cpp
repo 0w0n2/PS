1d8468cd87f0be5a686057774d3d1bf0f49cb002
Hyewon Lee <dahxtq1@naver.com>
2026-09-21 07:56:17 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 int solution(int n, int a, int b) {
+    // (a-1)XOR(b-1)에서 가장 높은 1비트가 만나는 라운드 결정
+    // 000 - 001 
+    int x = (a-1) ^ (b-1); // xor
+    
     int step = 0;
-    while (a!=b) {
-        a = (a+1)/2;
-        b = (b+1)/2;
-        
+    while (x) { // x!=0 될 때까지 가장 높은 1비트를 찾아서
         step++;
+        x >>= 1;
     }
     
     return step;
